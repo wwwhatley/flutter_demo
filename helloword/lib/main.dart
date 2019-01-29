47e9fc2fa4d61d5d6d4647cbb66150b01e424602
William Whatley <william@catatek.com>
2019-01-29 13:25:43 -0500

    Remove unused var.

--- helloword/lib/main.dart
+++ helloword/lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
